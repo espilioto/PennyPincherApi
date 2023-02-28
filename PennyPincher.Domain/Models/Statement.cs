b5cca21fc4e9aff2971b4d13027d0eeb7405462c
unknown <espilioto@gmail.com>
2023-03-01 00:36:41 +0200

    Added legacy column for v1 api :back: compatibility

--- PennyPincher.Domain/Models/Statement.cs
+++ PennyPincher.Domain/Models/Statement.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PennyPincher.Domain.Models
 {
@@ -7,6 +9,7 @@ namespace PennyPincher.Domain.Models
         public int Id { get; set; }
         [Required] public DateTime Date { get; set; }
         [Required] public int AmountInCents { get; set; }
+        [Required] [Column(TypeName = "decimal(8, 2)")] public decimal Amount { get; set; }
         [Required] public string Description { get; set; } = string.Empty;
         [Required] public int CategoryId { get; set; }
 
