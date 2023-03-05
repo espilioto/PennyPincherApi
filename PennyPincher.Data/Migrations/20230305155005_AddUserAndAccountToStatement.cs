7542ac3bb075df75723fe812f3addcf5e40e5ffb
unknown <espilioto@gmail.com>
2023-03-05 17:50:48 +0200

    Added user and account references to the statement entity

---
+++ PennyPincher.Data/Migrations/20230305155005_AddUserAndAccountToStatement.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PennyPincher.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAndAccountToStatement : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "Statements",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Statements",
+                type: "varchar(255)",
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statements_AccountId",
+                table: "Statements",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Statements_UserId",
+                table: "Statements",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Statements_Accounts_AccountId",
+                table: "Statements",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Statements_AspNetUsers_UserId",
+                table: "Statements",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Statements_Accounts_AccountId",
+                table: "Statements");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Statements_AspNetUsers_UserId",
+                table: "Statements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Statements_AccountId",
+                table: "Statements");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Statements_UserId",
+                table: "Statements");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "Statements");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Statements");
+        }
+    }
+}
--- PennyPincher.Domain/Models/Statement.cs
+++ PennyPincher.Domain/Models/Statement.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -8,11 +9,15 @@ namespace PennyPincher.Domain.Models
     {
         public int Id { get; set; }
         [Required] public DateTime Date { get; set; }
+        [Required] public int AccountId { get; set; }
         [Required] public int AmountInCents { get; set; }
         [Required] [Column(TypeName = "decimal(8, 2)")] public decimal Amount { get; set; }
         [Required] public string Description { get; set; } = string.Empty;
         [Required] public int CategoryId { get; set; }
+        [Required] public string UserId { get; set; } = string.Empty;
 
         public virtual Category? Category { get; set; }
+        public virtual Account? Account { get; set; }
+        public virtual IdentityUser? User { get; set; }
     }
 }
